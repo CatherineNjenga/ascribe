db5744b81670f75701301177d1696868ab80afac
Catherine Njenga <freshiah.catherine@gmail.com>
2023-05-12 12:44:16 +0300

    Adding styles to the public templates

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 'use strict';
-const { home } = require('../controllers/main');
+const { home, about } = require('../controllers/main');
 const express = require('express'),
   router = express.Router();
 
 
-router.get('/', home);
+router.route('/').get(home);
+router.route('/about').get(about);
 
 module.exports = router;
