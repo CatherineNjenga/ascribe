f9d79c02c0429f96c6db42d232467c4273878e24
Catherine Njenga <freshiah.catherine@gmail.com>
2023-05-20 11:21:38 +0300

    Populating data in the templates using context object data from our User model

--- ascribe.js
+++ ascribe.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const connectDB = require('./db/connect');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const mainRouter = require('./routes/main');
+const { connect } = require('mongoose');
 
 app.use(express.static('public'));
 
@@ -19,6 +21,19 @@ app.set('view engine', 'ejs');
 // routes
 app.use('/', mainRouter);
 
-app.listen(port, () => {
-  console.log(`Server listening on port: ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server listening on port: ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+    
+
+
