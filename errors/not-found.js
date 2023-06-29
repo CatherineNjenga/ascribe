988f3f9b8a0f1aae347f0fe537b994198e091b05
Catherine Njenga <freshiah.catherine@gmail.com>
2023-06-29 15:08:50 +0300

    Adding error handling middlewar

---
+++ errors/not-found.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { CustomError } = require('./custom-api');
+const { StatusCodes } = require('http-status-codes');
+
+class NotFoundError extends CustomError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
+
+module.exports = NotFoundError;
